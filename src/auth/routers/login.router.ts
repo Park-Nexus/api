a4342e1870176cec75a1cf57bf525c4f81004b66
vclong2003 <vclong2003@gmail.com>
2024-09-26 14:07:43 +0700

    refactor: Refactor login router to improve code organization and error handling

--- src/auth/routers/login.router.ts
+++ src/auth/routers/login.router.ts
@@ -8,48 +8,43 @@ import { comparePassword } from "../utils/password.utils";
 const inputSchema = z.object({
   email: z
     .string()
-    .regex(
-      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      "Invalid email format"
-    ),
+    .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid email format"),
   password: z.string().min(8, "Password must be at least 8 characters"),
 });
 
-export const loginRouter = procedure
-  .input(inputSchema)
-  .mutation(async ({ input }) => {
-    const { email, password } = input;
-    const account = await prisma.account.findUnique({
-      where: { email },
-    });
-
-    if (!account) {
-      throw new TRPCError({ code: "NOT_FOUND", message: "User not found" });
-    }
-
-    const hash = account.password;
-    const isPasswordValid = await comparePassword(password, hash);
-    if (!isPasswordValid) {
-      throw new TRPCError({
-        code: "UNAUTHORIZED",
-        message: "Invalid password",
-      });
-    }
-
-    const refreshToken = genRefreshToken({
-      id: account.id,
-    });
-    const accessToken = genAccessToken({
-      id: account.id,
-      role: account.role,
-    });
-
-    await prisma.accountToken.create({
-      data: {
-        token: refreshToken,
-        accountId: account.id,
-      },
-    });
-
-    return { accessToken, refreshToken };
+export const loginRouter = procedure.input(inputSchema).mutation(async ({ input }) => {
+  const { email, password } = input;
+  const account = await prisma.account.findUnique({
+    where: { email: email.toLowerCase() },
   });
+
+  if (!account) {
+    throw new TRPCError({ code: "NOT_FOUND", message: "User not found" });
+  }
+
+  const hash = account.password;
+  const isPasswordValid = await comparePassword(password, hash);
+  if (!isPasswordValid) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "Invalid password",
+    });
+  }
+
+  const refreshToken = genRefreshToken({
+    id: account.id,
+  });
+  const accessToken = genAccessToken({
+    id: account.id,
+    role: account.role,
+  });
+
+  await prisma.accountToken.create({
+    data: {
+      token: refreshToken,
+      accountId: account.id,
+    },
+  });
+
+  return { accessToken, refreshToken };
+});
