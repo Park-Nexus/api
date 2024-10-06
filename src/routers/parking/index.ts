45d9eec6b5985a783ded547ffa3848910d198272
vclong2003 <vclong2003@gmail.com>
2024-10-06 14:28:11 +0700

    refactor: Add getMany method to parking lot router

--- src/routers/parking/index.ts
+++ src/routers/parking/index.ts
@@ -3,7 +3,7 @@ import { trpcInstance } from "../../trpc";
 import * as lot from "./index.parkingLot";
 
 export const parkingRouter = trpcInstance.router({
-  lot: { submit: lot.submit },
+  lot: { submit: lot.submit, get: { many: lot.getMany } },
   spot: {},
   record: {},
 });
