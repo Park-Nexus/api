be8cd151dbf4e4dd6bc053d62ad1f1a6a459810d
vclong2003 <vclong2003@gmail.com>
2024-11-22 17:57:10 +0700

    test

---
+++ src/utils/oneSignal/index.ts
@@ -0,0 +1,7 @@
+import * as OneSignal from "@onesignal/node-onesignal";
+
+const configuration = OneSignal.createConfiguration({
+  restApiKey:
+    "os_v2_app_owkq2xgszzbwlbkjtvwuyfre52crk4hnqbzuodnprwugldehdz55onycolsbmdjpkuxsqdee4uj3rzbrduzywh5oslzzt2275fjh7cq",
+  userAuthKey: "",
+});
