1d73987e6e4af185d06adab844cc25a4156ec4ef
vclong2003 <vclong2003@gmail.com>
2024-12-02 17:40:39 +0700

    add types

--- src/auth/routers/index.ts
+++ src/auth/routers/index.ts
@@ -1,11 +1,11 @@
 import { trpcInstance } from "../../trpc";
 import { getAccessTokenRouter } from "./getAccessToken.router";
 import { getAuthState } from "./getAuthState.router";
-import { loginRouter } from "./login.router";
+import { adminLoginRouter, loginRouter, verifyLoginRouter } from "./login.router";
 import { registerRouter } from "./register.router";
 
 export const authRouter = trpcInstance.router({
-  login: loginRouter,
+  login: { user: loginRouter, admin: adminLoginRouter, verify: verifyLoginRouter },
   register: registerRouter,
   getAccessToken: getAccessTokenRouter,
   getAuthState: getAuthState,
